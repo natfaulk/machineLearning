3f2996f190eb836dfc88667a7bb08a171c1f8e08
natfaulk <nfaulk@hotmail.co.uk>
2016-05-20 11:20:45 +1200

    added keypress code

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,40 @@ int main(int, char**){
 
 	game.myChar.setPos(100,100);
 
+	bool leftPressed = false;
+	bool rightPressed = false;
+
 	while(!quit){
 		//Handle events on queue
-		 while( SDL_PollEvent( &e ) != 0 )
-		 {
-				 //User requests quit
-				 if( e.type == SDL_QUIT )
-				 {
-						 quit = true;
-				 }
-		 }
+		while( SDL_PollEvent( &e ) != 0 ) {
+			//User requests quit
+			if( e.type == SDL_QUIT ) {
+				quit = true;
 
-		 game.update();
-		 game.render();
+			} else if( e.type == SDL_KEYDOWN ) {
+				if(e.key.keysym.sym == SDLK_LEFT){
+					leftPressed = true;
+				} else if(e.key.keysym.sym == SDLK_RIGHT){
+					rightPressed = true;
+				}
+			} else if( e.type == SDL_KEYUP ) {
+				if(e.key.keysym.sym == SDLK_LEFT){
+					leftPressed = false;
+				} else if(e.key.keysym.sym == SDLK_RIGHT){
+					rightPressed = false;
+				}
+			}
+		}
+
+		if (leftPressed){
+			game.myChar.setVelocity(game.myChar.getXvelocity()-0.1,game.myChar.getYvelocity());
+		}
+		if (rightPressed){
+			game.myChar.setVelocity(game.myChar.getXvelocity()+0.1,game.myChar.getYvelocity());
+		}
+
+		game.update();
+		game.render();
 	}
 
 
